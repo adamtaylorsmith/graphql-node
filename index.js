a33c6213fa27762eb7b234075e2f7401f5638f6a
adamtaylorsmith <adamtaylor@icloud.com>
2022-10-18 17:34:29 -0400

    connected to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const express = require('express')
-require('dotenv').config()
-const { graphqlHTTP } = require('express-graphql')
-const schema = require('./schema/schema')
-const app = express()
-const PORT = process.env.PORT || 5000
-// const userData = require('./MOCK_DATA')
+const express = require('express');
+const colors = require('colors');
+// const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+connectDB();
+
+// app.use(cors());
+
+// Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+var root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+};
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+    // rootValue: root,
+    graphiql: true,
+    // graphiql: process.env.NODE_ENV === 'development'
+  })
+);
 
-app.listen(PORT, () => {
-    console.log('Server listening on ' + PORT)
-})
+app.listen(port, console.log(`Server running on port ${port}`));
+console.log('Running a GraphQL API server');
--- schema/schema.js
+++ schema/schema.js
@@ -1,34 +1,33 @@
-// const Project = require('../models/Project');
-// const Client = require('../models/Client');
-
+// const { projects, clients } = require('../sampleData')
+const Project = require('../models/Project')
+const Client = require('../models/Client')
 const {
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLSchema,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLEnumType,
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLList,
+//   GraphQLNonNull,
+//   GraphQLEnumType,
 } = require('graphql');
 
-// // Project Type
-// const ProjectType = new GraphQLObjectType({
-//   name: 'Project',
-//   fields: () => ({
-//     id: { type: GraphQLID },
-//     name: { type: GraphQLString },
-//     description: { type: GraphQLString },
-//     status: { type: GraphQLString },
-//     client: {
-//       type: ClientType,
-//       resolve(parent, args) {
-//         return Client.findById(parent.clientId);
-//       },
-//     },
-//   }),
-// });
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                // return clients.find(client => client.id === parent.clientId);
+                Client.findById(parent.clientId)
+            },
+        }, 
+    }),
+});
 
-// Client Type
 const ClientType = new GraphQLObjectType({
   name: 'Client',
   fields: () => ({
@@ -42,39 +41,37 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return Project.find()
+            },
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // return projects.find((project) => project.id === args.id)
+                return Project.findById(args.id)
+            },
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id)
-                // return Client.findById(args.id);
+                // return clients.find(client => client.id === args.id)
+                return Client.findById(args.id);
+            },
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return Client.find();
             },
         },
     }
 })
 
-//     projects: {
-//       type: new GraphQLList(ProjectType),
-//       resolve(parent, args) {
-//         return Project.find();
-//       },
-//     },
-//     project: {
-//       type: ProjectType,
-//       args: { id: { type: GraphQLID } },
-//       resolve(parent, args) {
-//         return Project.findById(args.id);
-//       },
-//     },
-//     clients: {
-//       type: new GraphQLList(ClientType),
-//       resolve(parent, args) {
-//         return Client.find();
-//       },
-//     },
-//   },
-
-// // Mutations
 // const mutation = new GraphQLObjectType({
 //   name: 'Mutation',
 //   fields: {
